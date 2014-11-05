5c02289c9d87697bde8e2dae7e473bda574ecd19
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-11-04 23:38:39 -0800

    Add prompt for IcedCoffeeScript.

--- demonstrate.sh
+++ demonstrate.sh
@@ -56,6 +56,7 @@ exec 3>&1
 case "$INTERPRETER" in
     bash    ) PROMPT='${GREEN}"demonstrating@$SCRIPT"${ENDC}" "${BLUE}$(pwd | python util/butlast.py)"\$"${ENDC}' ;;
     coffee  ) PROMPT='coffee>' ;;
+    iced    ) PROMPT='iced>' ;;
     irb     ) N=1; PROMPT='"irb(main):"$(printf "%03d" $N)":0>"; N=$((N + 1))' ;;
     node    ) PROMPT='>' ;;
     python* ) PROMPT='>>>' ;;
